5c6486aea7b77db90af23cfd36b6294c6639e26c
Pratik Dash <dashpratik40@gmail.com>
2022-11-18 06:25:08 +0530

    Feat - Converted into a GUI

--- app.js
+++ app.js
@@ -1,78 +1,151 @@
+//UI
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorBtn = document.querySelector(".scissor");
+const computerBtns = document.querySelectorAll(".computerBtns");
+const UIplayerScore = document.querySelector(".playerScoreValue");
+const UIcomputerScore = document.querySelector(".computerScoreValue");
+const resultbox = document.querySelector(".resultBox");
+const gameStatus = document.createElement("h3");
+const nameInput = document.querySelector("#name");
+const UIplayerName = document.querySelector(".playerName");
+const beginGameButton = document.querySelector(".begin");
+const gameBox= document.querySelector(".gameBox");
+const finalResult  = document.querySelector(".finalResult");
+const resetBtn = document.querySelector(".reset");
+resetBtn.style.display = "none";
+gameBox.style.display = "none";
+resultbox.appendChild(gameStatus);
+//Game Scores
 let playerScore = 0;
 let computerScore = 0;
+let round = 0;
+ function SetPlayerAndComputerScores(winnerOfRound){
+    if(winnerOfRound === "Player"){
+        playerScore++;
+    }
+    else if(winnerOfRound == "Computer"){
+        computerScore++;
+    }
 
-function updateScore(winnerOfRound){
-
-if(winnerOfRound.toLowerCase() === "player"){
-    playerScore++;
+    UIplayerScore.innerText = playerScore;
+    UIcomputerScore.innerText = computerScore;
 }
-else if(winnerOfRound.toLowerCase() === "computer"){
-
-    computerScore++;
-}
-else{
-    playerScore+=0;
-    computerScore+=0;
-}
-
-}
+    
+//playerButtons
+rockBtn.addEventListener("click",playRound);
+paperBtn.addEventListener("click", playRound);
+scissorBtn.addEventListener("click", playRound);
 
 
-function getRandomChoiceFromComputer(){
+function GetRandomValueFromComputer(){
 
     let choices = ["Rock","Paper","Scissor"];
     return choices[Math.floor(Math.random()*choices.length)];
+
 }
+function performUIChangesForComputer(computerSelection){
 
-function playRound(playerSelection,computerSelection){
+    let buttonNumber = 0;
+   for(let index = 0;index<computerBtns.length;index++){
 
+        if(computerBtns[index].innerText.toLowerCase() === computerSelection.toLowerCase()){
+            buttonNumber = index;
+            break
+        }
+   }
+    computerBtns[buttonNumber].focus();
+}
+function playRound(e){
+
+    let playerSelection = e.srcElement.value;
+    let computerSelection = GetRandomValueFromComputer();
+    performUIChangesForComputer(computerSelection);
     let winnerOfRound;
+     if(playerSelection.toLowerCase()=="rock" && computerSelection.toLowerCase() == "scissor"||
+     playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock"||
+     playerSelection.toLowerCase() == "scissor" && computerSelection.toLowerCase() == "paper"
+     ){
+         gameStatus.innerText = `You won. ${playerSelection} beats ${computerSelection}`
+         winnerOfRound = "Player"
+         SetPlayerAndComputerScores(winnerOfRound);
+        
+        
+      }
+      else if(
+
+                 playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "rock"||
+                 playerSelection.toLowerCase() == "scissor" && computerSelection.toLowerCase() == "scissor"||
+                playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "paper"
+                
+             ){
+                 gameStatus.innerText = "This round was a tie"
+                 winnerOfRound = "none"
+                 SetPlayerAndComputerScores(winnerOfRound);
+             }
+        else{
+                
+                gameStatus.innerText = "You lost this round."
+                winnerOfRound = "Computer"
+                SetPlayerAndComputerScores(winnerOfRound);
+                         
+             }
+    round++;
+    updateRound()
+
+}
+
+function updateRound(){
+    if(round == 5){
+        if(playerScore>computerScore){
+            finalResult.style.display = "block";
+            finalResult.innerText = `Bravo, you won the game.`;
+            gameBox.style.display = "none";
+            beginGameButton.innerText = "Play again?";
+            resetBtn.style.display = "block";
+            
+
+        }
+        else if(playerScore<computerScore){
+            finalResult.style.display = "block"
+           finalResult.innerText = `Oh no! you lost try again.`;
+           gameBox.style.display = "none";
+           resetBtn.style.display = "block"
+           
+        }
+        else{
+            finalResult.style.display = "block"
+            finalResult.innerText = `The game was a tie.`;
+           gameBox.style.display = "none";
+           beginGameButton.innerText = "Play again?"
+           resetBtn.style.display = "block"
+           
+        }
+    }
+}
+
+function resetGame(){
+
+    gameStatus.innerText = "";
+    playerScore = 0;
+    computerScore = 0;
+    finalResult.innerText = "";
+    round = 0;
+    UIcomputerScore.innerText = 0;
+    UIplayerScore.innerText = 0;
+    showGameUI();
+    resetBtn.style.display = "none";
    
-    if(playerSelection.toLowerCase()=="rock" && computerSelection.toLowerCase() == "scissor"||
-    playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock"||
-    playerSelection.toLowerCase() == "scissor" && computerSelection.toLowerCase() == "paper"
-    ){
-        console.log("You won this round")
-        winnerOfRound = "player"
-        updateScore(winnerOfRound)
-        
-    }
-    else if(
-
-        playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "rock"||
-        playerSelection.toLowerCase() == "scissor" && computerSelection.toLowerCase() == "scissor"||
-        playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "paper"
-        
-    ){
-        console.log("This round was a tie")
-        winnerOfRound = "none"
-        updateScore(winnerOfRound)
-    }
-    else{
-        console.log("You lost this round.")
-        winnerOfRound = "computer"
-        updateScore(winnerOfRound)
-    }
 }
+resetBtn.addEventListener("click",resetGame);
 
-function game(){
+function showGameUI(){
 
-    for(let i = 0; i<5; i++){
-
-        let userSelection = prompt("Select your weapon -> Rock, Paper or Scissor");
-        let computerSelection = getRandomChoiceFromComputer();
-        console.log("Your choice = ",userSelection+" Computer choice = "+computerSelection)
-        playRound(userSelection,computerSelection)
-    }
-
-    if(playerScore > computerScore){
-        console.log("Congrats! you won the game")
-    }else if(computerScore > playerScore){
-        console.log("Oh no! you lost the game.")
-    }
-    else{
-        console.log("Nobody won, nobody lost, the game was a tie.")
-    }
+    gameBox.style.display = "block";
+    beginGameButton.style.display = "none";
+    let playerName = nameInput.value;
+    UIplayerName.innerText = playerName+"'s";
+    nameInput.style.display = "none";
+    
 }
-game();
-
+beginGameButton.addEventListener("click",showGameUI);
